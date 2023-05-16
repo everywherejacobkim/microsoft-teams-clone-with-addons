32d00808b81e292f305839d8c97de79cb8550cd4
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-05-16 22:55:31 +0900

    Build: create sidebar component & styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/header';
 import './App.css';
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Header username='Jacob Kim'/>
+      <Header username='Jacob Kim' />
+      <Sidebar />
     </div>
   );
 }
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { FaRegBell } from 'react-icons/fa';
+import { BsChatText } from 'react-icons/bs';
+import { IoCalendarOutline } from 'react-icons/io5';
 
-type Props = {}
-
-const Sidebar = (props: Props) => {
+const Sidebar: React.FC = () => {
   return (
-    <div>Sidebar</div>
+    <div className="flex flex-col h-screen w-24 bg-gray-200 text-gray-700">
+      <nav className="flex-1">
+        <ul className="mt-4">
+          <li className="flex flex-col items-center justify-center px-4 py-4 hover:bg-white hover:text-primary hover:cursor-pointer">
+              <FaRegBell className="w-6 h-6" />
+              <p>Activity</p>
+          </li>
+          <li className="flex flex-col items-center justify-center px-4 py-4 hover:bg-white hover:text-primary hover:cursor-pointer">
+              <BsChatText className="w-6 h-6" />
+              <p>Chat</p>
+          </li>
+          <li className="flex flex-col items-center justify-center px-4 py-4 hover:bg-white hover:text-primary hover:cursor-pointer">
+              <IoCalendarOutline className="w-6 h-6" />
+              <p>Calendar</p>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
-}
+};
 
-export default Sidebar
+export default Sidebar;
